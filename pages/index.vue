59c1ea4f7f85b6f85611b60a9558a06e229acc15
Holden Malinchock <h.malinchock@verizon.net>
2023-10-30 23:21:30 -0400

    Start pickleball tournaments replacement

---
+++ pages/index.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <div>
+      My new first component
+    </div>
+    <button @click="r++">Click me</button>
+    <div>
+      {{ r }}
+    </div>
+    <UCard >
+    <template #header />
+    Body
+    <template #footer />
+  </UCard>
+  <UButton>Button</UButton>
+    </div>
+</template>
+
+<script setup>
+const r = ref(0)
+</script>
